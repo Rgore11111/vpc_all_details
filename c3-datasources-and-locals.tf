db81524dac48ba2bdfceb28096320023842c3a45
Rajesh Gore <rajesh.gore5@gmail.com>
2026-01-15 14:59:58 +0530

    Add VPC Terraform module

--- c3-datasources-and-locals.tf
+++ c3-datasources-and-locals.tf
@@ -1,9 +1,11 @@
-data "aws_availability_zone" "available" {
+# Datasources
+data "aws_availability_zones" "available" {
   state = "available"
-
 }
+
+# Locals Block
 locals {
-  azs=slice(data.aws_availability_zone.available.name,0,3)
-  public_subnet=[for k,az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets  = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
   private_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)]
 }
